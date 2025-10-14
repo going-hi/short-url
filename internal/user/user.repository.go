fbd342ed3fb1e871b8736abd916e4879811c9618
Going <goinghiggvp@gmail.com>
2025-10-15 00:03:33 +0300

    user repository

--- internal/user/user.repository.go
+++ internal/user/user.repository.go
@@ -6,6 +6,44 @@ import (
 
 
 type UserRepository struct {
-	db *sql.DB
+	Db *sql.DB
 }
 
+func (r *UserRepository) FindByEmail(email string) (*User, error) {
+	u := &User{}
+	query := `SELECT id, email, password FROM users WHERE email=$1`
+	err := r.Db.QueryRow(query, email).Scan(
+		u.Id,
+		u.Email,
+		u.Password,
+	)
+
+	if err != nil {
+
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+
+		return nil, err
+	}
+
+	return u, nil
+}
+
+func (r *UserRepository) Create(email, hashPassword string) (*User, error) {
+	query := `INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id, email, password`
+
+	u := &User{}
+
+	err := r.Db.QueryRow(query, email, hashPassword).Scan(
+		u.Id,
+		u.Email,
+		u.Password,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return u, nil
+}
